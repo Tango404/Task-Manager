ec57f6cf81384e3854be8c81bcdf2c83a0ca3f7b
Arjun Saini <arjuns1026@gmail.com>
2023-06-03 17:10:59 -0400

    Basic setup of controllers and routes

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const routes = require('./routes/tasks');
+const tasks = require('./routes/tasks');
+
+const port = 3000;
+
+// Middleware
+app.use(express.json()); // If we dont use this we wont have the data in the body of the request
+
+// Routes
+app.get('/hello', (req, res) => {
+	res.send('Task Manager App');
+});
+
+// This is the ROUTE for the TASKS ROUTER (routes/tasks.js)
+app.use('/api/v1/tasks', tasks);
+
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}...`);
+});
---
+++ controllers/tasks.js
@@ -0,0 +1,27 @@
+const getAllTasks = (req, res) => {
+	res.json(req.body);
+};
+
+const createTask = (req, res) => {
+	res.json(req.body);
+};
+
+const getTask = (req, res) => {
+	res.json(req.body);
+};
+
+const updateTask = (req, res) => {
+	res.json(req.body);
+};
+
+const deleteTask = (req, res) => {
+	res.json(req.body);
+};
+
+module.exports = {
+	getAllTasks,
+	createTask,
+	getTask,
+	updateTask,
+	deleteTask,
+};
